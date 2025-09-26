44b2bf3cc75499c4bf5e54d8500b13300ee7fab1
Geming400 <Geming400@gmail.com>
2025-09-26 23:37:56 +0200

    updated gitignore to ignore `test.py`

--- src/test.py
+++ src/test.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from pprint import pprint
 import tempfile
 from types import FrameType
-from typing import Optional
+from typing import Optional, TypeVar
 import aiosqlite
 import dotenv
 import os
@@ -28,7 +28,40 @@ async def test():
     # await perms.save()
     # print("Saved")
 
+T = TypeVar('T')
+
+def splitList(l: list[T], n: int, index: int) -> list[T]:
+    """Splits a list in members of `n` and returns the index `index`
+
+    Args:
+        l: The list to split
+        n: The numbers of sub-list to make
+        index: The index of the sublist to get
+        
+    Returns:
+        The sublist of index `index`
+    """
+    
+    finalLists: list[list[T]] = []
+    currentSubList: list[T] = []
+    
+    for i, v in enumerate(l, start=1):
+        currentSubList.append(v)
+        
+        if i % n == 0:
+            finalLists.append(currentSubList.copy())
+            currentSubList.clear()
+    
+    if currentSubList != []:
+        finalLists.append(currentSubList.copy())
+        currentSubList.clear()
+    
+    return finalLists[index]
+
 #asyncio.run(test())
-pprint(CONFIG.getStatuses())
+#pprint(CONFIG.getStatuses())
+
+print(splitList([10, 20, 30, 40, 50], 1, 0))
+
 print("finished")
 sys.exit(0)
